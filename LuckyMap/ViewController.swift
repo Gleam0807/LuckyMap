61ccfccec920880251b239c4ffce70482557c5cd
gleam0807 <teemocscsg@naver.com>
2024-05-16 13:32:58 +0900

    Initial Commit

---
+++ LuckyMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LuckyMap
+//
+//  Created by SUNG on 5/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
